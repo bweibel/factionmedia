a6d44d8cbc6846ab87a910d99b695bb93022c224
Ben Weibel <weibel@gmail.com>
2020-07-26 20:54:25 -0700

    Add video hero component

---
+++ template-parts/content/video_hero.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying a post's header
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+<section class="video-hero">
+  <div class="overlay"></div>
+  <iframe src="https://player.vimeo.com/video/385736203" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+</section><!-- .video-background -->
