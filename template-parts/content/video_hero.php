db23767ffd7e8ad06a48af2faca5587289a1042f
Ben Weibel <weibel@gmail.com>
2020-07-28 09:07:07 -0700

    add placeholder video

--- template-parts/content/video_hero.php
+++ template-parts/content/video_hero.php
@@ -10,6 +10,10 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <section class="video-hero">
-  <div class="overlay"></div>
-  <iframe src="https://player.vimeo.com/video/385736203" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+  <!-- <div class="overlay">
+  </div> -->
+  <div class="video-wrapper" style="padding:56.25% 0 0 0;position:relative;">
+    <iframe src="https://player.vimeo.com/video/385736203?autoplay=1&loop=1&title=0&byline=0&portrait=0&background=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" autoplay="true" allowfullscreen></iframe>
+  </div>
+  <script src="https://player.vimeo.com/api/player.js"></script>
 </section><!-- .video-background -->
